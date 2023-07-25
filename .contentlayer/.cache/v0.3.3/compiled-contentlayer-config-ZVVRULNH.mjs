44882101680e6f9d2f328e58a5cff67c4a221e44
Raj <rajvishwakarma303@gmail.com>
2023-07-26 02:49:20 +0530

    update

---
+++ .contentlayer/.cache/v0.3.3/compiled-contentlayer-config-ZVVRULNH.mjs
@@ -0,0 +1,36 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `posts/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    date: {
+      type: "date",
+      required: true
+    }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => `/${doc._raw.sourceFileName.replace(/\.mdx$/, "")}`
+    },
+    slugAsParams: {
+      type: "string",
+      resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/")
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "./content",
+  documentTypes: [Post]
+});
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-ZVVRULNH.mjs.map
